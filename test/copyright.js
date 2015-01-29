e0b8ebe11a40a6124bb175100357b8d7d2789f8f
Felix Schlitter <felixschlitter@gmail.com>
2015-01-29 23:05:41 +1300

    Add test for filtered copying

--- test/copyright.js
+++ test/copyright.js
@@ -2,6 +2,7 @@ var assert       = require('assert')
   , fs           = require('fs')
   , path         = require('path')
   , rimraf       = require('rimraf')
+  , _            = require('lodash')
   , readDirFiles = require('read-dir-files')
   , copyright    = require('../lib/copyright.js')
 ;
@@ -27,6 +28,39 @@ describe('copyright', function () {
                     readDirFiles(out, 'utf8', true, function (outErr, outFiles) {
                         assert.ifError(srcErr);
                         assert.deepEqual(srcFiles, outFiles);
+                        done();
+                    });
+                });
+            });
+        });
+
+        describe('when copying files using filter', function () {
+            before(function (done) {
+                rimraf(out, function () {
+                    copyright.copyFolder(
+                          src
+                        , out
+                        , { filter: function(name) {
+                            return _.last(name) != 'a'
+                          } }
+                    ).then(function() { done(); });
+                });
+            });
+
+            it('files are copied correctly', function (done) {
+                readDirFiles(src, 'utf8', true, function (srcErr, srcFiles) {
+
+                    var filtered = function(xs) {
+                        return _.omit(_.mapValues(xs, function(file, filename) {
+                            return file instanceof Object
+                                ? filtered(file)
+                                : _.last(filename) == 'a' ? undefined : file
+                        }), function(v) { return v === undefined; });
+                    }
+
+                    readDirFiles(out, 'utf8', function (outErr, outFiles) {
+                        assert.ifError(outErr);
+                        assert.deepEqual(filtered(srcFiles), outFiles);
                         done();
                     });
                 });
