af5e875883a8f40e2d8d8a93313b4feef3536e3d
Felix Schlitter <felixschlitter@gmail.com>
2015-09-19 09:36:47 +1200

    Use `async/await` in `mkdirs`

--- src/mkdirs.js
+++ src/mkdirs.js
@@ -17,18 +17,18 @@ const debug = _debug('kopeer');
  * @returns {Promise}
  * Returns the unit promise.
  */
-export default Bluebird.coroutine(function*(directory) {
+export default async function(directory) {
   const maxTries = 10;
-  yield _.foldl(
+  await _.foldl(
     _.tail(directory.split(path.sep))
-  , (acc, seg) => acc.then(Bluebird.coroutine(function*(curpath) {
+  , (acc, seg) => acc.then(async (curpath) => {
       const _curpath = ([
         (curpath === null) ? _.head(directory.split(path.sep)) : curpath
         , seg
       ]).join(path.sep);
 
       let tries = 0
-      yield (function mkdir() {
+      await (function mkdir() {
         debug(
           `Creating directory \`${ _curpath }\` `
         + `(Try { tries + 1 } / $ { maxTries })`);
@@ -52,6 +52,6 @@ export default Bluebird.coroutine(function*(directory) {
           });
       })();
       return _curpath;
-    }))
+    })
   , Bluebird.resolve(null));
-});
+};
