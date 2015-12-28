71d36962baba8834a926e4f6400106b6e874b4c5
Felix Schlitter <felixschlitter@gmail.com>
2015-12-28 17:17:59 +1300

    Remove watch task

--- gulpfile.js
+++ gulpfile.js
@@ -33,21 +33,4 @@ gulp.task('feature',    inc('minor'));
 gulp.task('prerelease', inc('prerelease'));
 gulp.task('release',    inc('major'));
 
-gulp.task('make:resilient', function () {
-  return gulp.src(['src/**/*.js'])
-    .pipe(sourcemaps.init())
-    .pipe(babel({ optional: [ 'bluebirdCoroutines' ] }))
-    .pipe(babel({ optional: [ 'runtime' ] }))
-    .on('error', function(error) {
-      console.error(error.stack);
-      this.emit('end');
-    })
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist'));
-});
-
 gulp.task('default', ['make']);
-
-gulp.task('watch', ['make'], function () {
-  gulp.watch('./src/**/*', ['make:resilient']);
-});
